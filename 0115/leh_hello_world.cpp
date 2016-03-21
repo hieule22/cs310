096b1270251df0f79424258411e9528d93254943
Hieu Le <htl5683@vh222004.truman.edu>
2016-03-21 14:27:33 -0500

    Added all assignment folders

---
+++ 0115/leh_hello_world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * A program to give a simple greeting to the terminal
+ * @author Hieu Le
+ * @version 14 January 2016
+ */
+
+int main()
+{
+  cout << "Hello, world!" << endl;
+  return 0;
+}
